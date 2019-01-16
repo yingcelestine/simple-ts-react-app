4de6cc4240288b0483dd62500f2eef3464cc88c3
Ying Liu <L.Yliuying@outlook.com>
2019-01-16 23:05:42 +0100

    change output directory to dist

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,24 @@ import Routes from "./Routes";
 const reactLogo = require("./assets/React-icon.png");
 
 const App = () => (
-  <BrowserRouter>
-    <main className="container">
-      <div>
-        <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working!</p>
-      </div>
-      <ul className="left">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <Routes />
-    </main>
-  </BrowserRouter>
+    <BrowserRouter>
+        <main className="container">
+            <div>
+                <h1>hello world!</h1>
+                <img className="container__image" alt="react logo" src={reactLogo} />
+                <p>If you see this everything is working!</p>
+            </div>
+            <ul className="left">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+            <Routes />
+        </main>
+    </BrowserRouter>
 );
 
 export default App;
